a58ce2cfa0018914703c7458f29cf7dc7bfa183f
Elli-PhD <eliana.sitelman@gmail.com>
2021-07-24 19:35:28 -0700

    Updates to query_blueprints

--- py/query_blueprints.py
+++ py/query_blueprints.py
@@ -4,7 +4,8 @@ import numpy as np
 import sys
 
 # Pull global dataframe of all products and materials
-all_mats = pd.read_csv('global - list of materials and products.csv')
+all_mats = pd.read_csv('global - list of materials and products.csv',\
+                       header = 0, encoding = 'unicode_escape')
 
 # Pull query information from .csv file and add to list
 query = pd.read_csv('query_blueprints - query list.csv')
@@ -12,21 +13,202 @@ query = query[query['Query'] == 'y']
 query['Quantity'] = query['Quantity'].astype(float)
 query['ME'] = query['ME'].astype(float)
 
-
 # Load all blueprint data
 blueprints = pd.read_csv('blueprints.csv')
 blueprints = blueprints[\
                 ['Quantity', 'Material Name', 'Product Name']]
 blueprints['Quantity'] = blueprints['Quantity'].astype(float)
 
-
 # Extract query data from blueprint data
-tier1 = pd.DataFrame()
+##tier1 = pd.DataFrame()
+##for i in query['Name'].to_list():
+##    tier1 = pd.concat([tier1, blueprints[\
+##                                blueprints['Product Name'] == i]])
+
+
+# This should be a recursive algorithm
+# Take the initial list of material requirements
+# Parse it into PI, Raw Materials, and Product lists
+# Repeat with subsequent Product lists, appending to PI and Raw Material lists until Product lists is empty
+base = pd.DataFrame()
+products = pd.DataFrame()
 for i in query['Name'].to_list():
-    tier1 = pd.concat([tier1, blueprints[\
+    base = pd.concat([base, blueprints[\
                                 blueprints['Product Name'] == i]])
-##print('Tier 1 List')
-##print(tier1)
+
+# Multiply base list by ME and quantities
+products = pd.DataFrame()
+for i in base['Product Name'].drop_duplicates():
+    tmp = base[base['Product Name'] == i].copy()
+    mef = query[query['Name'] == i]['ME'].iloc[0]
+    quantity = query[query['Name'] == i]['Quantity'].iloc[0]
+##    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity)
+##    tmp['Quantity'] = tmp['Quantity']*quantity*(1-mef/100.)
+    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity*(1-mef/100.))
+    products = pd.concat([products, tmp])
+
+# Add unique material quantities together
+products_tmp = []
+for i in products['Material Name'].drop_duplicates():
+    tmp = products[products['Material Name'] == i].copy()
+    mat = tmp['Material Name'].drop_duplicates().tolist()[0]
+    total_quantity = np.sum(tmp['Quantity'])
+    products_tmp.append([total_quantity, mat])
+products_tmp = pd.DataFrame(products_tmp)
+products_tmp.columns = ['Quantity','Material Name']
+products = products_tmp
+
+##products = base
+iteration = 0
+print('Base list (iteration = %s):'%iteration)
+print(base)
+print('\n\n')
+print('Product list (Iteration = %s, length = %s)'%(iteration,len(products)))
+print(products)
+print('\n\n')
+
+# In products, extract PI, Raw materials, and products
+# Take for products, query building materials and append to PI, Raw Materials, and generate new products
+# repeat until list contains only raw materials
+# Seperate list of Planetary Industry materials from other materials
+pi_list = products[products['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == True]
+# Seperate raw materials from products
+raw_materials = products[products['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == True]
+# Seperate Items that require further breakdown
+products = products[(products['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == False) & \
+                   (products['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == False)]
+
+print('Planetary Industry (iteration = %s):'%iteration)
+print(pi_list)
+print('\n\n')
+print('Other raw materials (iteration = %s):'%iteration)
+print(raw_materials)
+print('\n\n')
+print("Remaining products (iteration = %s, length = %s)"%(iteration, len(products)))
+print(products)
+print('\n\n')
+
+# Iterative process starts here
+# Generate Base List
+base_tmp = pd.DataFrame()
+for i in products['Material Name'].to_list():
+    base_tmp = pd.concat([base_tmp, blueprints[\
+                                blueprints['Product Name'] == i]])
+iteration =+ 1
+print('Base (base_tmp, iteration = %s)'%iteration)
+print(base_tmp)
+# Multiply each Material by number of associated products and ME (assume ME=10)
+products_tmp = pd.DataFrame()
+for i in base_tmp['Product Name'].drop_duplicates():
+    tmp = base_tmp[base_tmp['Product Name'] == i].copy()
+    quantity = products[products['Material Name'] == i]['Quantity'].iloc[0]
+    print(quantity)
+##    # Keep this for future reference; I may need to make a list of ME values in the future
+##    mef = query[query['Name'] == i]['ME'].iloc[0]
+##    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity*(1-mef/100.))
+    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity*0.9)
+    products_tmp = pd.concat([products_tmp, tmp])
+products = products_tmp
+print('Base times quantities and ME (products, iteration = %s)'%iteration)
+print(products)
+print('\n\n')
+
+# Add unique material quantities together
+products_tmp = []
+for i in products['Material Name'].drop_duplicates():
+    tmp = products[products['Material Name'] == i].copy()
+    mat = tmp['Material Name'].drop_duplicates().tolist()[0]
+    total_quantity = np.sum(tmp['Quantity'])
+    products_tmp.append([total_quantity, mat])
+products = pd.DataFrame(products_tmp)
+products.columns = ['Quantity','Material Name']
+print('products (iteration = %s)'%iteration)
+print(products)
+
+# Place contents of products_tmp into respective [tmp] dataframes (PI, Raw, and Products)
+# variables to recall: pi_list, raw_materials
+# Seperate list of Planetary Industry materials from other materials
+pi_list_tmp = products[products['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == True]
+# Seperate raw materials from products
+raw_materials_tmp = products[products['Material Name'].isin(all_mats['Tier 1'].tolist())\
+                == True]
+# Seperate Items that require further breakdown
+products_tmp = products[(products['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == False) & \
+                   (products['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == False)]
+print('\n\n Working section')
+##print(pi_list_tmp)
+##print(raw_materials_tmp)
+
+# Append tmp contents to base contents (PI and Raw)
+pi_list = pd.concat([pi_list, pi_list_tmp])
+raw_materials = pd.concat([raw_materials, raw_materials_tmp])
+##print(pi_list)
+##print(raw_materials)
+
+# Add unique material quantities together
+pi_list_tmp = pd.DataFrame()
+for i in pi_list['Material Name'].drop_duplicates():
+    tmp = pi_list[pi_list['Material Name'] == i].copy()
+    mat = tmp['Material Name'].drop_duplicates().tolist()[0]
+    total_quantity = np.sum(tmp['Quantity'])
+    pi_list_tmp.append([total_quantity, mat])
+products = pd.DataFrame(products_tmp)
+products.columns = ['Quantity','Material Name']
+print('products (iteration = %s)'%iteration)
+print(products)
+
+
+sys.exit()
+
+
+
+sys.exit()
+# Seperate list of Planetary Industry materials from other materials
+pi_list = tier3[tier3['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == True]
+# Seperate raw materials from products
+raw_materials = tier3[tier3['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == True]
+# Seperate Items that require further breakdown
+to_process = tier3[(tier3['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == False) & \
+                   (tier3['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == False)]
+
+
+
+
+
+
+
+##while len(products) != 0:
+# Multiply 'Material Name' lists by quantities per item and
+# Material Efficiency Factor and round up
+# Item quantities configured in query file
+tier2 = pd.DataFrame()
+for i in products['Product Name'].drop_duplicates():
+    tmp = products[products['Product Name'] == i].copy()
+    mef = query[query['Name'] == i]['ME'].iloc[0]
+    quantity = query[query['Name'] == i]['Quantity'].iloc[0]
+##    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity)
+##    tmp['Quantity'] = tmp['Quantity']*quantity*(1-mef/100.)
+    tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity*(1-mef/100.))
+    tier2 = pd.concat([tier2, tmp])
+print(tier2)
+
+sys.exit()
+
+
+
+# Extract PI-specific information
+
 
 # Save list of materials (w/o duplicates)
 # Useful for generating/updating the following file:
@@ -48,8 +230,8 @@ for i in tier1['Product Name'].drop_duplicates():
 ##    tmp['Quantity'] = tmp['Quantity']*quantity*(1-mef/100.)
     tmp['Quantity'] = np.ceil(tmp['Quantity']*quantity*(1-mef/100.))
     tier2 = pd.concat([tier2, tmp])
-print(tier1[['Quantity','Material Name']])
-print(tier2)
+##print(tier2)
+##print('\n\n')
 ##sys.exit()
 
 # Add unique material quantities together
@@ -61,19 +243,62 @@ for i in tier2['Material Name'].drop_duplicates():
     tier3.append([total_quantity, mat])
 tier3 = pd.DataFrame(tier3)
 tier3.columns = ['Quantity','Material Name']
+
+
+
+
+# Seperate list of Planetary Industry materials from other materials
+pi_list = tier3[tier3['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == True]
+# Seperate raw materials from products
+raw_materials = tier3[tier3['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == True]
+# Seperate Items that require further breakdown
+to_process = tier3[(tier3['Material Name'].isin(all_mats['PI-all-items'].tolist())\
+                == False) & \
+                   (tier3['Material Name'].isin(all_mats['Raw material name'].tolist())\
+                == False)]
+
+# Identify materials required to build each product
+tier2 = pd.DataFrame()
+for i in to_process['Material Name'].to_list():
+    tier2 = pd.concat([tier2, blueprints[\
+                                blueprints['Product Name'] == i]])
+
+# Multiply by ME and quantities
+
+
+
+print('Query List')
+print(query[['Name', 'Quantity']])
+print('\n\n')
+print('Tier 1 (non-PI)')
 print(tier3)
+print('\n\n')
+print("Tier 1 (PI)")
+print(pi_list)
+print('\n\n')
+print("Tier 1 (raw materials)")
+print(raw_materials)
+print('\n\n')
+print("Tier 1 (Items that require identifying raw materials)")
+print(to_process)
+print('\n\n')
+print("Tier 2")
+print(tier2[''])
+print('\n\n')
+
 
 
 
 ########################################################
 # Print list of items that are related to Planetary Industry
 ########################################################
-print("Planetary Industry Items - Tier 1")
-for i in tier3['Material Name'].drop_duplicates():
-    if i in all_mats['PI'].to_list():
-        print(i)
-    else:
-        pass
+##for i in pi_list['Material Name'].drop_duplicates():
+##    if i in all_mats['PI-all-items'].to_list():
+##        print(i)
+##    else:
+##        pass
 sys.exit()
 
 # parse tier3 dataframe
